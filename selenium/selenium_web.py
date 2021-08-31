dba6facb8d5bad75c9ff39f255b461bccb8b2939
Pritesh lathia <72033803+pritesh-1998@users.noreply.github.com>
2021-09-01 01:19:26 +0530

    gg

--- selenium/selenium_web.py
+++ selenium/selenium_web.py
@@ -1,16 +1,18 @@
 import details
 from selenium import webdriver
-accesstype = ""
-repo_name=""
-print(" 1) Do you want to import already existed repo ")
-print(" 2) Do you want to Create anew one")
-work=int(input("Select any one from the above :- "))
-def common_questions():
-        repo_name=input("Enter the desired repo name :- ")
-        accesstype=input("Do you want your Repo to be public or private :- ")
+# accesstype = ""
+# repo_name=""
+# print(" 1) Do you want to import already existed repo ")
+# print(" 2) Do you want to Create anew one")
+# work=int(input("Select any one from the above :- "))
+# # def common_questions():
+#         repo_name=input("Enter the desired repo name :- ")
+#         accesstype=input("Do you want your Repo to be public or private :- ")
 
 def creation_repo():
-        common_questions()
+        # common_questions()
+        repo_name=input("Enter the desired repo name :- ")
+        accesstype=input("Do you want your Repo to be public or private :- ")
         desc=input("Do you want to insert description (T/F):- ")
         if desc== "T":
             desc_data=input("Description = ")
@@ -50,7 +52,6 @@ def creation_repo():
         if desc== "True":
             desc_button=browser.find_element_by_id("repository_description")
             desc_button.send_keys(desc_data)
-        selection_private_public_repo()
         #selecting type of gitignore file
         if gitignore== "T":
             gitignore_selector=browser.find_element_by_id("repository_gitignore_template_toggle")
@@ -58,9 +59,7 @@ def creation_repo():
             #finding element using xpath
             gitignore_type_key=browser.find_element_by_xpath("//span[.='None']")
             gitignore_type_key.click()
-            if gitignore_type== "Blank":
-                pass
-            elif gitignore_type=="Vsc":
+            if gitignore_type=="Vsc":
                 gitignore_type_vsc_key=browser.find_element_by_xpath("//span[.='VisualStudio']")
                 #gitignore_type_vsc_key=browser.find_element_by_id("repository_gitignore_template_VisualStudio")
                 gitignore_type_vsc_key.click()
@@ -77,46 +76,48 @@ def creation_repo():
             update_button=browser.find_element_by_link_text("Update")
             update_button.click()
 
-def import_repo():
-    url=input("Your old repository’s clone URL :- ")
-    common_questions()
-    browser = webdriver.Chrome()
-    browser.get("https://github.com/")
+#def import_repo():
+url=input("Your old repository’s clone URL :- ")
+# common_questions()
+repo_name=input("Enter the desired repo name :- ")
+accesstype=input("Do you want your Repo to be public or private :- ")
 
-    # finding element by text
-    sign_in_link = browser.find_element_by_link_text("Sign in")
+browser = webdriver.Chrome()
+browser.get("https://github.com/")
 
-    # clicking that element
-    sign_in_link.click()
+# finding element by text
+sign_in_link = browser.find_element_by_link_text("Sign in")
 
-    # finding element on webpage and select it using class name
-    username_box = browser.find_element_by_id("login_field")
-    password_box = browser.find_element_by_id("password")
+# clicking that element
+sign_in_link.click()
 
-    # stimulating a user typing text
-    username_box.send_keys(details.username)
-    password_box.send_keys(details.password)
-    password_box.submit()
-    browser.get("https://github.com/new/import")
-    clone_url=browser.find_element_by_id("vcs_url")
-    clone_url.click()
-    clone_url.send_keys(url)
-    #input a repo name
-    repo_name_field= browser.find_element_by_id("repository_name")
-    repo_name_field.send_keys(repo_name)
-    #private or public
-    if accesstype=="public":
-        public_key=browser.find_element_by_id("repository_visibility_public")
-        public_key.click()
+# finding element on webpage and select it using class name
+username_box = browser.find_element_by_id("login_field")
+password_box = browser.find_element_by_id("password")
 
-    elif accesstype=="private":
-        private_key=browser.find_element_by_id("repository_visibility_private")
-        private_key.click()
+# stimulating a user typing text
+username_box.send_keys(details.username)
+password_box.send_keys(details.password)
+password_box.submit()
+browser.get("https://github.com/new/import")
+clone_url=browser.find_element_by_id("vcs_url")
+clone_url.click()
+clone_url.send_keys(url)
+#input a repo name
+repo_name_field= browser.find_element_by_id("repository_name")
+repo_name_field.send_keys(repo_name)
+#private or public
+if accesstype=="public":
+    public_key=browser.find_element_by_id("repository_visibility_public")
+    public_key.click()
 
-    # begin=browser.find_elements_by_tag_name('button')
-    # begin.click()
+elif accesstype=="private":
+    private_key=browser.find_element_by_id("repository_visibility_private")
+    private_key.click()
+begin_in_link =browser.find_element_by_xpath("input[@value='Begin import']")
+begin_in_link.click()
 
-if work==1:
-    import_repo()
-elif work==2:
-    creation_repo()
+# if work==1:
+#     import_repo()
+# elif work==2:
+#     creation_repo()
